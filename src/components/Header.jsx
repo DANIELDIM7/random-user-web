cac61a3610d5f24ee1d314ab34c8d275d3dfca25
Daniel Cuellar <ingdanielcuellar4737@gmail.com>
2022-11-08 13:44:29 -0500

    En esta versión se agregaron los componentes Link de react-dom como componentes de Link de materiaL-UI Así le dimos vida a los enlaces del HEADER

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,6 +7,7 @@ import {
   Link,
   styled,
 } from "@mui/material";
+import { Link as RouterLink } from "react-router-dom";
 
 // Estilos de la Toolbar
 
@@ -23,21 +24,25 @@ function Header() {
           {/* Container ayuda a centrar el contenido del componenteen este caso de la Toolbar,maxWidth y le puedo decir el tamaño de pantalla que puede adoptar con maxWidth */}
           <ToolbarSlider>
             <IconButton edge="start">
-              {/* Dege es la posición que va a tener el ícono, en este caso es en el inicio */}
+              {/* edge es la posición que va a tener el ícono, en este caso es en el inicio */}
               <img src="logo512.png" alt="Logo" height="30px" />
             </IconButton>
-            <Link href="" color="inherit">
+
+            <Link underline="hover" component={RouterLink} to="/" href="" color="inherit">
               Inicio
             </Link>
-            <Link href="" color="inherit">
+            {/* Así haremos que el componente Link material ui se comporte como un RouterLink de REact Dom */}
+
+            <Link underline="hover" component={RouterLink} to="/Form" color="inherit">
               Formulario
             </Link>
-            <Link href="" color="inherit">
+
+            <Link underline="hover" component={RouterLink} to="/List" color="inherit">
               Lista
             </Link>
           </ToolbarSlider>
         </Container>
-        {/* position sticky es para que se quede en la parte de arriba */}
+       
       </AppBar>
     </div>
   );
