3e3b23d79806a7ceb0c46d6170161ae0ae29f8be
Daniel Cuellar <ingdanielcuellar4737@gmail.com>
2022-11-14 23:16:00 -0500

    empecé a trabajar con una API FAKE desde el un servidor local, se usaron los hooks useEffect y las formas asincronas para traer los datos de la Api

--- src/pages/List.jsx
+++ src/pages/List.jsx
@@ -1,10 +1,32 @@
-import React, { useState } from "react";
-import userList from "../components/data";
+import React, { useEffect, useState } from "react";
+// import userList from "../components/data";
 import ItemUser from "../components/itemUser";
 import Title from "../components/Title";
+import axios from "axios";
 
 const List = () => {
-  const [users, setUsers] = useState(userList);
+  const [users, setUsers] = useState([]);
+
+  const getData = async ()=>{
+    try {
+      const response = await axios.get('http://localhost:3000/data') 
+      const {users} = response.data
+      console.log(response)
+      console.log(users)
+      setUsers(users)
+
+    }catch (error){
+      console.log(error)
+    }
+  }
+
+  // Quiero que la función getdata se ejecute cuando se renderice para eso usamos un Hook
+
+  useEffect(() =>{
+    getData()
+  },[])
+
+
 
   return (
     <>
